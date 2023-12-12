d394f25a6f8d27f366208a46871bc425d7ddb495
diashvili <114678389+diashvili@users.noreply.github.com>
2023-12-12 17:08:28 +0400

    Add project files.

---
+++ TaskManagmentSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskManagmentSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
